a7b8631d5e5f496ec9c8fabceeca48b07b4c980a
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-15 16:49:21 -0500

    Added z variable to troubleshoot clicked object

--- DragMirror.cs
+++ DragMirror.cs
@@ -7,25 +7,32 @@ public class DragMirror : MonoBehaviour
   private Vector2 mousePosition;
 
   private float offsetX, offsetY;
+  [SerializeField]
+  private float zPos = 2f;
 
+  void Start()
+  {
+    Debug.Log("STARTING MIRROR POS: " + transform.position);
+    transform.position = new Vector3(transform.position.x, transform.position.y, zPos);
+
+  }
 
   void Update()
   {
     Debug.Log("mousePosition");
+    Debug.Log("CURRENT MIRROR POS: " + transform.position);
 
   }
   void OnMouseDown()
   {
-    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     Debug.Log("MOUSE DOWN ON MIRROR");
 
   }
 
   private void OnMouseDrag()
   {
-    Debug.Log("MOUSE DRAG ON MIRROR");
-
-    transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    transform.position = new Vector3(mousePosition.x, mousePosition.y, zPos);
 
   }
 
