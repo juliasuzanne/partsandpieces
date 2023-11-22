59c62fec14b8ff973ae5b43ce7ec5ad5bb1acd44
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-22 13:40:26 -0500

    Show and hide scene players on mirror panel load due to raycast interference

--- Cave/ColorChangePanel.cs
+++ Cave/ColorChangePanel.cs
@@ -10,10 +10,18 @@ public class ColorChangePanel : MonoBehaviour
     private GameObject _afterChangePanel;
     [SerializeField]
     private GameObject _returnPanel;
+    [SerializeField]
+    private GameObject _abovePlayer;
+    [SerializeField]
+    private GameObject _rockFace;
     // Start is called before the first frame update
     void Start()
     {
+        _abovePlayer = GameObject.Find("AbovePlayer");
+        _rockFace = GameObject.Find("Set_Background").transform.GetChild(3).transform.GetChild(1).gameObject;
         _afterChangePanel.SetActive(true);
+        _abovePlayer.SetActive(false);
+        _rockFace.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,6 +32,8 @@ public class ColorChangePanel : MonoBehaviour
 
     public void ResumeGame()
     {
+        _rockFace.SetActive(true);
+        _abovePlayer.SetActive(true);
         _returnPanel.SetActive(true);
         _rockBridge.SetActive(false);
         this.gameObject.SetActive(false);
