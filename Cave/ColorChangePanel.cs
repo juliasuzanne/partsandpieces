d5eb2a21224c5611d21d4e09b15731db930fd7cc
Julia Grimes <julia.s.grimes@gmail.com>
2023-11-21 22:21:19 -0500

    Adjust transition at the end

--- Cave/ColorChangePanel.cs
+++ Cave/ColorChangePanel.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ColorChangePanel : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _rockBridge;
+    [SerializeField]
+    private GameObject _afterChangePanel;
+    [SerializeField]
+    private GameObject _returnPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _afterChangePanel.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void ResumeGame()
+    {
+        _returnPanel.SetActive(true);
+        _rockBridge.SetActive(false);
+        this.gameObject.SetActive(false);
+
     }
 }
