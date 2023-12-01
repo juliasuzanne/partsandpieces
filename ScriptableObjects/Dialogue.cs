9cc8042450005b5429b63583a0e11390ab4e9731
Julia Grimes <julia.s.grimes@gmail.com>
2023-12-01 05:55:12 -0500

    Reorganize scripts, create dialogue scriptable object

---
+++ ScriptableObjects/Dialogue.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue Object", order = 0)]
+public class Dialogue : ScriptableObject
+{
+
+}
