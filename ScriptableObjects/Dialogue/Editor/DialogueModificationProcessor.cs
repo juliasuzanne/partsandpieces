613d4c2f740a4218bec47f73e6cf72f982ebe78e
Julia Grimes <julia.s.grimes@gmail.com>
2023-12-07 13:03:32 -0500

    Fix rename s.o. bug

---
+++ ScriptableObjects/Dialogue/Editor/DialogueModificationProcessor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+namespace Dialogue.Editor
+{
+    public class DialogueModificationProcessor : UnityEditor.AssetModificationProcessor
+    {
+        private static AssetMoveResult OnWillMoveAsset(string sourcePath, string destinationPath)
+        {
+            Dialogue dialogue = AssetDatabase.LoadMainAssetAtPath(sourcePath) as Dialogue;
+            if (dialogue == null)
+            {
+                return AssetMoveResult.DidNotMove;
+            }
+            if (Path.GetDirectoryName(sourcePath) != sourcePath.GetDirectoryName(destinationPath))
+            {
+                return AssetMoveResult.DidNotMove;
+            }
+
+            dialogue.name = Path.GetFileNameWithoutExtension(destinationPath);
+
+            return AssetMoveResult.DidNotMove;
+
+        }
+    }
+
+}
