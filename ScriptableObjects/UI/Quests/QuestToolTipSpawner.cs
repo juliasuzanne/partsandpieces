8a1433f5f869a913321a1f3d61147d737e56c868
Julia Grimes <julia.s.grimes@gmail.com>
2023-12-10 12:42:27 -0500

    Using script to access abstract class tooltipspawner to show tooltips on quest UI hover

---
+++ ScriptableObjects/UI/Quests/QuestToolTipSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameDevTV.Core.UI.Tooltips;
+
+public class QuestToolTipSpawner : TooltipSpawner
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
