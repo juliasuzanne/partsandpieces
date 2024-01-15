ffe7b088b806dc5b8b3a9d436c77aebd1c63bdd9
Julia Grimes <julia.s.grimes@gmail.com>
2024-01-15 00:06:34 -0500

    Remove commented out irrelevant code

--- Puzzles/SlidePuzzlePiece.cs
+++ Puzzles/SlidePuzzlePiece.cs
@@ -8,23 +8,11 @@ public class SlidePuzzlePiece : MonoBehaviour
     [SerializeField] private int correctNum;
     [SerializeField] private Vector2 correctPos;
 
-    // [SerializeField] private Vector2 onetwo = new Vector2(1, 0);
-    // [SerializeField] private Vector2 onethree = new Vector2(2, 0);
-    // [SerializeField] private Vector2 twoone = new Vector2(0, -1);
-    // [SerializeField] private Vector2 twotwo = new Vector2(1, -1);
-    // [SerializeField] private Vector2 twothree = new Vector2(2, -1);
-    // [SerializeField] private Vector2 threeone = new Vector2(0, -2);
-    // [SerializeField] private Vector2 threetwo = new Vector2(1, -2);
-    // [SerializeField] private Vector2 emptyPos = new Vector2(2, -2);
-
-
-    // Start is called before the first frame update
     void Start()
     {
         controlPiece = transform.parent.GetComponent<SlideEmptyPiece>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
