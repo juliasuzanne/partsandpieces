2a2eaa74917eb9bcb57de69b807ee8d0a0cabacb
Julia Grimes <julia.s.grimes@gmail.com>
2024-01-24 23:02:07 -0500

    Access sprite renderer for unearthing change appearance on click

--- General/UnearthSpawner.cs
+++ General/UnearthSpawner.cs
@@ -6,10 +6,17 @@ public class UnearthSpawner : MonoBehaviour
 {
     //spawn a random item from list on click
     private Vector2 posToSpawn;
+    private SpriteRenderer _sp;
     [SerializeField] private GameObject[] _items;
+
+    void Start()
+    {
+        _sp = transform.GetComponent<SpriteRenderer>();
+    }
 
     void OnMouseDown()
     {
+
         posToSpawn = new Vector2(transform.position.x + 2f, transform.position.y);
         GameObject newItem = Instantiate(_items[Random.Range(0, _items.Length)], posToSpawn, Quaternion.identity);
 
