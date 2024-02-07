14d95152ed3417b6db796dd91b928b55316d4be5
Julia Grimes <julia.s.grimes@gmail.com>
2024-02-06 23:12:02 -0500

    Add clock to manage game time

---
+++ Abstract/TimeManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public const int hoursInDay = 24, minutesInHour = 60;
+
+    public float dayDuration = 30f;
+    public Text message;
+
+    float totalTime = 0;
+    float currentTime = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime += Time.deltaTime;
+        currentTime = totalTime % dayDuration;
+        if (Mathf.FloorToInt(GetHour()) == 1)
+        {
+            message.text = "It's 1 something!";
+        }
+        else
+        {
+            message.text = "Nothing doing ...";
+        }
+
+    }
+
+    public float GetHour()
+    {
+        return currentTime * hoursInDay / dayDuration;
+    }
+
+    public float GetMinutes()
+    {
+        return (currentTime * hoursInDay * minutesInHour / dayDuration) % minutesInHour;
+    }
+
+    public string Clock24Hour()
+    {
+        //00:00
+        return Mathf.FloorToInt(GetHour()).ToString("00") + ":" + Mathf.FloorToInt(GetMinutes()).ToString("00");
+    }
+
+    public string Clock12Hour()
+    {
+        int hour = Mathf.FloorToInt(GetHour());
+        string abbreviation = "AM";
+
+        if (hour >= 12)
+        {
+            abbreviation = "PM";
+            hour -= 12;
+        }
+
+        if (hour == 0) hour = 12;
+
+
+
+        return hour.ToString("00") + ":" + Mathf.FloorToInt(GetMinutes()).ToString("00") + " " + abbreviation;
+    }
+}
